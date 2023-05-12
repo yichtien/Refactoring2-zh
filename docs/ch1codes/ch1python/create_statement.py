8fbbb7648b3f8bdae2f1c5cdd145faf42adb23a8
huajin <tien4yich@gmail.com>
2023-05-12 18:21:36 +0800

    [MOD]: separate into two files( two stages).

--- docs/ch1codes/ch1python/create_statement.py
+++ docs/ch1codes/ch1python/create_statement.py
@@ -1,13 +1,58 @@
-
-def enrich_performance(performance):
-    res = performance.copy()
-    res['play'] = play_for(performance)
-    res['amount'] = amount_for(performance)
-    res['volume_credits'] = volume_credits_for(performance)
-    return res
+import locale
+import math
 
 
-def create_statement(invoice, play):
+def create_statement_data(invoice, plays):
+    def amount_for(performance) -> float:
+        if play_for(performance)['type'] == 'tragedy':
+            res = 40000
+            if performance['audience'] > 30:
+                res += 1000 * (performance['audience'] - 30)
+        elif play_for(performance)['type'] == 'comedy':
+            res = 30000
+            if performance['audience'] > 20:
+                res += 10000 + 500 * (performance['audience'] - 20)
+            res += 300 * performance['audience']
+        else:
+            raise RuntimeError(f'unknown type: {play_for(performance)["type"]}')
+        return res
+
+    def play_for(performance):
+        """用以移除变量 `play` """
+        return plays[performance['playID']]
+
+    def volume_credits_for(perf):
+        # add volume credits
+        res: int = 0
+        res += max(perf['audience'] - 30, 0)
+        # add extra credit for every ten comedy attendees
+        if "comedy" == play_for(perf)['type']:
+            res += math.floor(perf['audience'] / 5)
+        return res
+
+    def usd(number):
+        locale.setlocale(locale.LC_ALL, 'en_US')
+        #
+        return locale.currency(number / 100, grouping=True)
+
+    def total_volume_credits(data):
+        res = 0
+        for perf in data['performances']:
+            res += perf['volume_credits']
+        return res
+
+    def total_amount(data):
+        res = 0
+        for perf in data['performances']:
+            res += perf['amount']
+        return res
+
+    def enrich_performance(performance):
+        res = performance.copy()
+        res['play'] = play_for(performance)
+        res['amount'] = amount_for(performance)
+        res['volume_credits'] = volume_credits_for(performance)
+        return res
 
     statement_data = {
         'customer': invoice['customer'],
@@ -15,3 +60,5 @@ def create_statement(invoice, play):
     }
     statement_data['total_amount'] = total_amount(statement_data)
     statement_data['total_volume_credits'] = total_volume_credits(statement_data)
+
+    return statement_data
--- docs/ch1codes/ch1python/statement.py
+++ docs/ch1codes/ch1python/statement.py
@@ -1,77 +1,34 @@
-import copy
-import locale
-import math
+from docs.ch1codes.ch1python.create_statement import usd, create_statement_data
 
 
 def statement(invoice, plays):
+    return render_plain_text(create_statement_data(invoice, plays))
 
-    def amount_for(performance) -> float:
-        if play_for(performance)['type'] == 'tragedy':
-            res = 40000
-            if performance['audience'] > 30:
-                res += 1000 * (performance['audience'] - 30)
-        elif play_for(performance)['type'] == 'comedy':
-            res = 30000
-            if performance['audience'] > 20:
-                res += 10000 + 500 * (performance['audience'] - 20)
-            res += 300 * performance['audience']
-        else:
-            raise RuntimeError(f'unknown type: {play_for(performance)["type"]}')
-        return res
 
-    def play_for(performance):
-        """用以移除变量 `play` """
-        return plays[performance['playID']]
+def render_plain_text(data):
+    result = f'Statement for {data["customer"]}\n'
 
-    def volume_credits_for(perf):
-        # add volume credits
-        res: int = 0
-        res += max(perf['audience'] - 30, 0)
-        # add extra credit for every ten comedy attendees
-        if "comedy" == play_for(perf)['type']:
-            res += math.floor(perf['audience'] / 5)
-        return res
+    for perf in data['performances']:
+        result += f'  {perf["play"]["name"]}: {usd(perf["amount"])} ({perf["audience"]} seats)\n'
 
-    def usd(number):
-        locale.setlocale(locale.LC_ALL, 'en_US')
-        #
-        return locale.currency(number / 100, grouping=True)
+    result += f'Amount owed is {usd(data["total_amount"])}\n'
+    result += f'You earned {data["total_volume_credits"]} credits\n'
+    return result
 
-    def total_volume_credits(data):
-        res = 0
-        for perf in data['performances']:
-            res += perf['volume_credits']
-        return res
 
-    def total_amount(data):
-        res = 0
-        for perf in data['performances']:
-            res += perf['amount']
-        return res
+def html_statement(invoice, plays):
+    return render_html(create_statement_data(invoice, plays))
 
-    def enrich_performance(performance):
-        res = performance.copy()
-        res['play'] = play_for(performance)
-        res['amount'] = amount_for(performance)
-        res['volume_credits'] = volume_credits_for(performance)
-        return res
 
-    def render_plain_text(data, invoice, plays):
+def render_html(data):
+    result = f'&lt;h1&gt;Statement for {data["customer"]}&lt;/h1&gt;\n'
+    result += f'&lt;table&gt;\n'
+    result += f'&lt;tr&gt;&lt;th&gt;play&lt;/th&gt;&lt;th&gt;seats&lt;/th&gt;&lt;th&gt;cost&lt;/th&gt;&lt;/tr&gt;'
+    for perf in data['performances']:
+        result += f'&lt;tr&gt;&lt;td&gt;{perf["play"]["name"]} &lt;/td &gt;&lt;td &gt;${perf.audience} &lt;/td&gt;'
+        result += f'&lt;td&gt;{usd(perf["amount"])}&lt;/td&gt;&lt;/tr&gt;\n'
+        result += '&lt;/table&gt;\n'
+        result += f'&lt;p&gt;Amount owed is &lt;em&gt;{usd(data["total_amount"])}&lt;/em&gt;&lt;/p&gt;\n'
+        result += f'&lt;p&gt;You earned&lt;em&gt;{data["total_volume_credits"]}&lt;/em&gt;credits&lt;/p&gt;\n'
 
-        result = f'Statement for {data["customer"]}\n'
-
-        for perf in data['performances']:
-            result += f'  {perf["play"]["name"]}: {usd(perf["amount"])} ({perf["audience"]} seats)\n'
-
-        result += f'Amount owed is {usd(data["total_amount"])}\n'
-        result += f'You earned {data["total_volume_credits"]} credits\n'
-        return result
-
-    statement_data = {
-        'customer': invoice['customer'],
-        'performances': list(map(enrich_performance, invoice['performances']))
-    }
-    statement_data['total_amount'] = total_amount(statement_data)
-    statement_data['total_volume_credits'] = total_volume_credits(statement_data)
-
-    return render_plain_text(statement_data, invoice, plays)
+    return result
